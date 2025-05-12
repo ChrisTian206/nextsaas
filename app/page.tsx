9e50326aeaf33935f5ba85351ddfe71b782a8d8a
Wenhao Tian <wtian16@bu.edu>
2025-05-12 15:03:58 -0700

    added gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Mainbody from "@/components/homepage/mainbody";
+import GradientBg from "@/components/common/GradientBg";
 export default function Home() {
   return (
-    <div className="relative w-full" >
-      <Mainbody />
+    <div className="relative w-full">
+      <GradientBg />
+      <div className="relative w-full" >
+        <Mainbody />
+      </div>
     </div>
+
   );
 }
