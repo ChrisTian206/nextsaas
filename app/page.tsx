29e559f0ea536b94a23841ff97ebda44aacb9a36
Wenhao Tian <wtian16@bu.edu>
2025-05-13 15:06:34 -0700

    added Demo section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Mainbody from "@/components/homepage/mainbody";
 import GradientBg from "@/components/common/GradientBg";
+import Demos from "@/components/homepage/Demos";
 export default function Home() {
   return (
     <div className="relative w-full">
       <GradientBg />
       <div className="relative w-full" >
         <Mainbody />
+        <Demos />
       </div>
     </div>
 
